8875a496b4a980ce940a551431a6cbe83706c505
Tim de Groot <tim.de.a.groot@sogeti.com>
2024-10-15 11:50:11 +0200

    Better help

--- arguments.py
+++ arguments.py
@@ -11,19 +11,19 @@ class ArgumentParser:
             "--version",
             action="store_true",
             dest="version",
-            help="Display application version information",
+            help="Display application version information.",
         )
         parser.add_argument(
             "-h",
             "--help",
-            help="Provide additional information",
+            help="Provide additional information.",
             action="help",
             default=argparse.SUPPRESS,
         )
         parser.add_argument(
             "-o",
             "--outputPath",
-            help="Specifies  1 or more paths to output.xml. \
+            help="`path` Specifies  1 or more paths to output.xml. \
                             Specify every XML separately with -o if you are providing more than one.",
             action="append",
             nargs="*",
@@ -32,7 +32,7 @@ class ArgumentParser:
         parser.add_argument(
             "-r",
             "--removeRuns",
-            help="Specifies 1 or more indexes or run_start datetimes to remove from the database. \
+            help="`string` Specifies 1 or more indexes or run_start datetimes to remove from the database. \
                             Specify every run separately with -r if you are providing more than one. \
                             Examples: -r 0 -r 1 -r 10 or --removeRuns '2024-07-30 15:27:20.184407' -r 20",
             action="append",
@@ -42,27 +42,27 @@ class ArgumentParser:
         parser.add_argument(
             "-d",
             "--databasePath",
-            help="Specifies the path to the database you want to \
+            help="`path` Specifies the path to the database you want to \
                             store the results in.",
             default=".\\robot_results.db",
         )
         parser.add_argument(
-            "-g",
-            "--generateDashboard",
-            help="Specifies if you want to generate the HTML \
-                            dashboard. Default is True, override if you only require the database.",
-            default=True,
-        )
-        parser.add_argument(
             "-n",
             "--nameDashboard",
-            help="Specifies a custom HTML dashboard name",
+            help="`path` Specifies a custom HTML dashboard name.",
             default="",
         )
         parser.add_argument(
             "-l",
             "--listRuns",
-            help="Specifies if the runs should be listed",
+            help="`boolean` Specifies if the runs should be listed. Default is True, override if you only require the database.",
+            default=True,
+        )
+        parser.add_argument(
+            "-g",
+            "--generateDashboard",
+            help="`boolean` Specifies if you want to generate the HTML \
+                            dashboard. Default is True, override if you only require the database.",
             default=True,
         )
         arguments = parser.parse_args()
