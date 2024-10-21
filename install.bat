6f2a9a07581bdf26716aa53c5e4c4e808ba1b503
Tim <tim-degroot@live.nl>
2024-10-21 22:26:19 +0200

    Updated install script commands

--- install.bat
+++ install.bat
@@ -1,9 +1,9 @@
-@REM pip install --upgrade pip setuptools build wheel twine
 pip install .
 rmdir /s /q .\dist\
 rmdir /s /q .\build\
 rmdir /s /q .\robotframework_dashboard.egg-info\
 
-@REM release commands
+@REM release packages and commands
+@REM python -m pip install --upgrade pip setuptools build wheel twine
 @REM python -m build
 @REM python -m twine upload --verbose --repository pypi dist/*
