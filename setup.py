76d29dde7d3997ccf2522ae9e88664b6750692f6
Tim <tim-degroot@live.nl>
2024-10-21 23:37:56 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="robotframework-dashboard",
-    version="0.1.2",
+    version="0.1.3",
     description="Output processor and dashboard generator for Robot Framework output files",
     long_description="""# Robot Framework Dashboard
 
