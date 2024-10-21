c18fc31f79c426489c1dc38fb39f47f1f6d7a80f
Tim de Groot <tim.de.a.groot@sogeti.com>
2024-10-21 19:10:35 +0200

    Added commands for releasing versions

--- install.bat
+++ install.bat
@@ -3,3 +3,7 @@ pip install .
 rmdir /s /q .\dist\
 rmdir /s /q .\build\
 rmdir /s /q .\robotframework_dashboard.egg-info\
+
+@REM release commands
+@REM python -m build
+@REM python -m twine upload --verbose --repository pypi dist/*
