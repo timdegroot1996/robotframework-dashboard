187045530406fec55e56956bf76572c45aa6c254
Tim de Groot <tim-degroot@live.nl>
2025-03-07 18:13:44 +0100

    Added removal of folders in release script

--- release.bat
+++ release.bat
@@ -4,3 +4,7 @@ python -m pip install --upgrade pip setuptools build wheel twine
 python -m build
 python -m twine upload --verbose --repository pypi dist/*
 @REM after this manually enter PYPI token
+
+rmdir /s /q .\dist\
+rmdir /s /q .\build\
+rmdir /s /q .\robotframework_dashboard.egg-info\
