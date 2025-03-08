b862ba9c176b51d858106ee41717414cd3ae6de0
Tim <tim-degroot@live.nl>
2025-03-08 22:49:03 +0100

    Removed unused line

--- release.bat
+++ release.bat
@@ -6,5 +6,4 @@ python -m twine upload --verbose --repository pypi dist/*
 @REM after this manually enter PYPI token
 
 rmdir /s /q .\dist\
-rmdir /s /q .\build\
 rmdir /s /q .\robotframework_dashboard.egg-info\
