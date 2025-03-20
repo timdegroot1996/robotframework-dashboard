7212bda266c7308d2662bcf049e55c1907411fac
Tim de Groot <tim-degroot@live.nl>
2025-03-21 00:27:24 +0100

    Added listener implementation and robot.toml examples

--- example/listener/robotdashboardlistener.py
+++ example/listener/robotdashboardlistener.py
@@ -1,45 +1,99 @@
 from sys import exit
 from os.path import exists
-from requests import post
+from requests import post, get, delete
 from requests.exceptions import ConnectionError
 
-# usage:
-# robot --listener path/to/robotdashboardlistener.py:output=path/to/output.xml:tags=tag1,tag2 path/to/tests.robot
+# for usage with a "robot.toml" see robot.toml in this folder
+
+# usage with robot command line:
+# robot --listener path/to/robotdashboardlistener.py:tags=tag1,tag2 path/to/tests.robot
 
 # most basic usage:
-# robot --listener robotdashboardlistener.py:output=./results/output.xml tests.robot
+# robot --listener robotdashboardlistener.py tests.robot
 # with run tags (comma separated):
-# robot --listener robotdashboardlistener.py:output=./results/output.xml:tags=prod,dev tests.robot
+# robot --listener robotdashboardlistener.py:tags=prod,dev tests.robot
 # with changed server host/port:
-# robot --listener robotdashboardlistener.py:output=./results/output.xml:tags=prod,dev:host=127.0.0.2:port=8544 tests.robot
+# robot --listener robotdashboardlistener.py:tags=prod,dev:host=127.0.0.2:port=8544 tests.robot
+
 
 class robotdashboardlistener:
 
-    def __init__(self, output: str, tags: str = None, host: str = "127.0.0.1", port: str = "8543"):
-        self.output = output
+    def __init__(
+        self,
+        tags: str = None,
+        host: str = "127.0.0.1",
+        port: str = "8543",
+        limit: str = "0",
+    ):
         self.host = host
         self.port = port
-        self.tags = tags.split(',') if tags != None else ['']
+        self.tags = tags.split(",") if tags != None else [""]
+        self.limit = int(limit)
 
-    def close(self):
-        if exists(self.output):
-            print(f"robotdashboardlistener: found output.xml '{self.output}', starting processing...")
-            body = {
-            "output_path": self.output,
-            "output_tags": self.tags
-            }
+    def output_file(self, path):
+        self._add_output_to_database(path=str(path))
+        self._remove_runs_over_limi()
+
+    def _add_output_to_database(self, path: str):
+        if exists(path):
+            self._print_listener(f"starting processing output.xml '{path}'")
+            body = {"output_path": path, "output_tags": self.tags}
             try:
-                response = post(f'http://{self.host}:{self.port}/add-outputs', json=body)
+                response = post(
+                    f"http://{self.host}:{self.port}/add-outputs", json=body
+                )
             except ConnectionError as e:
-                print(f'robotdashboardlistener: ERROR the server is not running or the url http://{self.host}:{self.port}/add-outputs is not correct!')
+                self._print_listener(
+                    f"ERROR the server is not running or the url http://{self.host}:{self.port}/add-outputs is not correct!"
+                )
                 exit(0)
             if response.status_code == 200:
-                message_lines = response.json()['console'].split('\n')
-                for message_line in message_lines:
-                    if len(message_line) == 0: continue
-                    print(f'robotdashboardlistener: {message_line}')
+                self._print_console_message(response)
             else:
-                print(f'robotdashboardlistener: ERROR something went wrong while sending results to the server')
-                print(f'robotdashboardlistener: {response.json()}')
-        else: 
-            print(f"robotdashboardlistener: ERROR could not find output.xml '{self.output}', skipped automatic processing")
+                self._print_listener(
+                    f"ERROR something went wrong while sending results to the server: {response.json()}"
+                )
+        else:
+            self._print_listener(
+                f"ERROR could not find output.xml '{path}', skipped automatic processing"
+            )
+
+    def _remove_runs_over_limi(self):
+        response = get(f"http://{self.host}:{self.port}/get-outputs")
+        if response.status_code == 200:
+            response_json = response.json()
+            self._print_listener(
+                f"amount of runs in the database: {len(response_json)}, limit {self.limit}"
+            )
+            if self.limit != 0 and len(response_json) > self.limit:
+                amount_to_remove = len(response_json) - self.limit
+                self._print_listener(
+                    f"removing {amount_to_remove} run(s) from the database"
+                )
+                run_starts = []
+                for i in range(0, amount_to_remove):
+                    run_starts.append(response_json[i]["run_start"])
+                body = {"runs": run_starts}
+                response = delete("http://127.0.0.1:8543/remove-outputs", json=body)
+                if response.status_code == 200:
+                    self._print_console_message(response)
+                else:
+                    self._print_listener(
+                        f"ERROR something went wrong while deleting the runs from the database: {response.json()}"
+                    )
+            else:
+                self._print_listener(f"no runs have to be removed")
+        else:
+            self._print_listener(
+                f"ERROR something went wrong while retrieving the outputs from the database: {response.json()}"
+            )
+
+    def _print_listener(self, value: str):
+        print(f"robotdashboardlistener: {value}")
+
+    def _print_console_message(self, response):
+        message_lines = response.json()["console"].split("\n")
+        for message_line in message_lines:
+            if len(message_line) == 0:
+                continue
+            self._print_listener(f"{message_line}")
