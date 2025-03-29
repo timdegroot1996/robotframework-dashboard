566bd046ca8e574041cc2684388dfc9db64d163d
Tim de Groot <tim-degroot@live.nl>
2025-03-29 01:01:54 +0100

    Added multi tag filter, upgraded version and fixed tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="robotframework-dashboard",
-    version="0.6.3",
+    version="0.6.4",
     description="Output processor and dashboard generator for Robot Framework output files",
     long_description="""# Robot Framework Dashboard
 
