d5343791f8b2bc4ee4dfa4a07757aab1e6c4f6fa
Tim de Groot <tim-degroot@live.nl>
2025-06-04 00:40:38 +0200

    Added heatmap graph and fixed donut drawing bug

--- robotframework_dashboard/version.py
+++ robotframework_dashboard/version.py
@@ -1,1 +1,1 @@
-__version__ = "Robotdashboard 0.9.2"
+__version__ = "Robotdashboard 0.9.3"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="robotframework-dashboard",
-    version="0.9.2",
+    version="0.9.3",
     description="Output processor and dashboard generator for Robot Framework output files",
     long_description="""# Robot Framework Dashboard
 
