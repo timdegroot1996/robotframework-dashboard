5ae79806f74553e64fe339a372e7a7aba49dcc27
Elias Haberl <ehaberl@tdt.de>
2025-11-12 17:11:56 +0100

    add class attribute RobotDashboard.project_version
    Signed-off-by: Elias Haberl <ehaberl@tdt.de>

--- robotframework_dashboard/robotdashboard.py
+++ robotframework_dashboard/robotdashboard.py
@@ -26,6 +26,7 @@ class RobotDashboard:
         message_config: list,
         quantity: int,
         use_logs: bool,
+        project_version: str,
     ):
         """Sets the parameters provided in the command line"""
         self.database_path = database_path
@@ -41,6 +42,7 @@ class RobotDashboard:
         self.use_logs = use_logs
         self.server = False
         self.database = None
+        self.project_version = project_version
 
     def initialize_database(self, suppress=True):
         """Function that initializes the database if it does not exist
