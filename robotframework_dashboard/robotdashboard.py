66885155de74605a6228494464979c77361c7d06
Elias Haberl <ehaberl@tdt.de>
2025-11-12 17:11:56 +0100

    add class attribute RobotDashboard.project_version
    Signed-off-by: Elias Haberl <ehaberl@tdt.de>

--- robotframework_dashboard/robotdashboard.py
+++ robotframework_dashboard/robotdashboard.py
@@ -27,6 +27,7 @@ class RobotDashboard:
         quantity: int,
         use_logs: bool,
         offline_dependencies: bool,
+        project_version: str,
     ):
         """Sets the parameters provided in the command line"""
         self.database_path = database_path
@@ -43,6 +44,7 @@ class RobotDashboard:
         self.offline_dependencies = offline_dependencies
         self.server = False
         self.database = None
+        self.project_version = project_version
 
     def initialize_database(self, suppress=True):
         """Function that initializes the database if it does not exist
