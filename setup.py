28a60d0323840cbc1d698ef0663d332f6a4af3f4
Tim de Groot <tim-degroot@live.nl>
2025-11-22 21:40:01 +0100

    Back to actual version 1.2.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extras["all"] = sorted({pkg for group in extras.values() for pkg in group})
 
 setup(
     name="robotframework-dashboard",
-    version="1.3.0",
+    version="1.2.2",
     description="Output processor and dashboard generator for Robot Framework output files",
     long_description="""# 📊 Robot Framework Dashboard
 
