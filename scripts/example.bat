dae3486124a6816bc30415db4cdf776af176084a
Tim de Groot <t.degroot@vgz.nl>
2025-11-28 18:12:41 +0100

    Updated scripts and added example dashboard and database for newest version

---
+++ scripts/example.bat
@@ -0,0 +1,15 @@
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002134.xml:prod:project_1 --projectversion 0.1
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002151.xml:dev:project_2 --projectversion 0.1
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002222.xml:prod:project_1 --projectversion 0.1
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002257.xml:dev:project_2 --projectversion 0.1
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002338.xml:prod:project_1 --projectversion 1.1
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002400.xml:dev:project_2 --projectversion 1.2
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002431.xml:prod:project_1 --projectversion 1.2
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002457.xml:dev:project_2 --projectversion 1.2
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002528.xml:prod:project_1 --projectversion 2.0
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002549.xml:dev:project_2 --projectversion 2.3
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002636.xml:prod:project_1 --projectversion 2.3
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002703.xml:dev:project_2 --projectversion 2.3
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002739.xml:prod:project_1
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002915.xml:prod:project_1 --projectversion 2.0
+robotdashboard -n robot_dashboard -o .\atest\resources\outputs\output-20250313-003006.xml:prod:project_1 --uselogs
